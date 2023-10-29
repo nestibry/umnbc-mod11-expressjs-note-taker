43c1355a791d56d2a29961e873b64769797fa736
nestibry <nestibry@gmail.com>
2023-10-29 08:21:45 -0500

    init server.js with dependencies

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+
+const PORT = 3001;
+
+app.use(express.static("public"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
