226da781bc375c6df471936660f54ae3880a5f9a
nestibry <nestibry@gmail.com>
2023-10-29 15:37:15 -0500

    successfully deletes items from the database from DELETE request

--- server.js
+++ server.js
@@ -63,43 +63,26 @@ app.post("/api/notes", (req, res) => {
     }
 });
 
+// Request to delete a note from database
 app.delete("/api/notes/:id", (req, res) => {
     // Log that a DELETE request was received
-    console.info(`${req.method} request received to add a new note`);
-
+    console.info(`${req.method} request received to delete note:`);
     const reqID = req.params.id;
-    console.log(reqID);
-
-    res.status(200).json({ status: "success", body: reqID});
-
-
-    // // Destructuring assignment for the items in req.body
-    // const { title, text } = req.body;
-
-    // // If all the required properties are present
-    // if (title && text) {
-    //     const newNote = {
-    //         id: uuidv4(),  // unique string id
-    //         title,
-    //         text,
-    //     };
+    
+    // Read notes from database 
+    fs.readFile(path.join(__dirname, databasePath), 'utf8', (err, data) => {
+        if( err ) return res.status(500).json({status: "error", body: 'Error reading from database'});
+        const database = JSON.parse(data);
         
-    //     // Read notes from database and Write newNote to database
-    //     fs.readFile(path.join(__dirname, databasePath), 'utf8', (err, data) => {
-    //         if( err ) return res.status(500).json({status: "error", body: 'Error reading database'});
-    //         let database = JSON.parse(data);
-            
-    //         // Write newNote to database
-    //         // database.push(newNote);
-    //         // fs.writeFile(path.join(__dirname, databasePath), JSON.stringify(database), (err) => {
-    //         //     if( err ) return res.status(500).json({status: "error", body: 'Error writing to database'} );
-    //         //     console.log(newNote);
-    //         //     res.status(200).json({ status: "success", body: newNote});
-    //         // });
-    //     });
-    // } else {
-    //     res.status(500).json({status: "error", body: 'Error posting title and text'});
-    // }
+        // Delete Item from Database
+        const deletedItem = database.filter( item => item.id === reqID);
+        const newDatabase = database.filter( item => item.id !== reqID);
+        fs.writeFile(path.join(__dirname, databasePath), JSON.stringify(newDatabase), (err) => {
+            if( err ) return res.status(500).json({status: "error", body: 'Error deleting item from database'} );
+            console.log(deletedItem);
+            res.status(200).json({ status: "success", body: deletedItem});
+        });
+    });
 });
 
 
