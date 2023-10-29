6a87b09809701a3c8c87ab842ef4a42aa0c43eb3
nestibry <nestibry@gmail.com>
2023-10-29 15:04:22 -0500

    intit the delete request...reqID logged

--- server.js
+++ server.js
@@ -63,7 +63,44 @@ app.post("/api/notes", (req, res) => {
     }
 });
 
-app.delete("/api/notes/:id", (req, res) => {});
+app.delete("/api/notes/:id", (req, res) => {
+    // Log that a DELETE request was received
+    console.info(`${req.method} request received to add a new note`);
+
+    const reqID = req.params.id;
+    console.log(reqID);
+
+    res.status(200).json({ status: "success", body: reqID});
+
+
+    // // Destructuring assignment for the items in req.body
+    // const { title, text } = req.body;
+
+    // // If all the required properties are present
+    // if (title && text) {
+    //     const newNote = {
+    //         id: uuidv4(),  // unique string id
+    //         title,
+    //         text,
+    //     };
+        
+    //     // Read notes from database and Write newNote to database
+    //     fs.readFile(path.join(__dirname, databasePath), 'utf8', (err, data) => {
+    //         if( err ) return res.status(500).json({status: "error", body: 'Error reading database'});
+    //         let database = JSON.parse(data);
+            
+    //         // Write newNote to database
+    //         // database.push(newNote);
+    //         // fs.writeFile(path.join(__dirname, databasePath), JSON.stringify(database), (err) => {
+    //         //     if( err ) return res.status(500).json({status: "error", body: 'Error writing to database'} );
+    //         //     console.log(newNote);
+    //         //     res.status(200).json({ status: "success", body: newNote});
+    //         // });
+    //     });
+    // } else {
+    //     res.status(500).json({status: "error", body: 'Error posting title and text'});
+    // }
+});
 
 
 // Send out the home page for all other GET requests
