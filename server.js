917b17e68588adfd1dbb9b9924ad011d4c102223
nestibry <nestibry@gmail.com>
2023-10-30 09:47:54 -0500

    updated to: const PORT = process.env.PORT || 3001

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const databasePath = "db/db.json";
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"));
 
